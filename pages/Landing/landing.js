2f50429c85e76c1634408040732bbcb01e8538ee
Kayphrosh <olakunbiolabode@yahoo.com>
2023-08-04 16:43:48 +0100

    fixed build issue

--- pages/Landing/landing.js
+++ pages/Landing/landing.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar/navbar'
 import Heroe from '@/components/LandingPage/HeroeSection/Heroe'
 import LatestPetition from '@/components/LandingPage/LatestPetitionSection/LatestPetition'
 import SuccessPetition from '@/components/LandingPage/LatestPetitionSection/SuccessPetition'
-import Faq from '@/components/LandingPage/FAQ/FAQ'
+import Faq from '@/components/LandingPage/FAQ/Faq'
 const Landing = () => {
   return (
     <section className='container'>
